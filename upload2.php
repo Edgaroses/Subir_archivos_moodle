4b77e14940b02700634b6c3a90b67fe5832b7a47
DESKTOP-7CRD9J8\Edgar Oses <edgar.oses@bnex.cl>
2016-11-09 10:16:32 -0300

    App para subir archivos
    Esta funcionando el .php.
    Carga los usuario del sistema moodle y selecicona su id.

---
+++ upload2.php
@@ -0,0 +1,53 @@
+<?php
+    $mysqli = new mysqli("localhost","root","","bnexcl_moodle");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <title>Subir archivos al servidor</title>
+    <meta name ="author" content ="Norfi Carrodeguas">
+    
+    <style type="text/css" media="screen">
+        body{font-size:1.2em;}
+    </style>
+            
+</head>
+    
+<body>
+    <h2>Subir Archivos</h2>
+    
+    <form enctype='multipart/form-data' action='php_subir.php' method='post'>
+        <fieldset>
+            <legend> Datos </legend>
+                <p>
+                    Seleccione Usuario:
+                    <select>
+                        <option value="0">Seleccione    un    Usuario</option>
+                        
+                        <?php
+										
+                            $query = $mysqli -> query ("SELECT id, firstname, lastname FROM mdlhj_user");
+
+                            while ($valores = mysqli_fetch_array($query)) {
+
+                            echo '<option value="'.$valores[id].'">'.$valores[firstname].'  '.$valores[lastname].'</option>';
+
+                          }
+                        ?>
+                        
+                    </select>
+                    
+                    <br>
+                    
+                    <input name='uploadedfile' type='file'>
+                    <input type='submit' value='Subir archivo'>
+                </p>
+        </fieldset>
+    </form>
+    
+
+</body>
+    
+</html>
